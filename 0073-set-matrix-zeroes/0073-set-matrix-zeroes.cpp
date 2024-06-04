b3334f80e65de9669666356f2fd09bee9d3dc8b5
Aditya Kamble <122638989+27-aditya@users.noreply.github.com>
2024-06-04 15:14:07 +0530

    Time: 18 ms (10.64%), Space: 16.4 MB (66.3%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,29 +1,53 @@
 class Solution {
-private:
-
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         int row = matrix.size();
         int col = matrix[0].size();
-        queue<pair<int, int>>q;
+
+        bool isfirstrow = false;
+        bool isfirstcol = false;
 
         for(int i = 0; i < row; i++){
-            for(int j = 0; j < col; j++){
+            if(matrix[i][0] == 0){
+                isfirstcol = true;
+                break;
+            }
+        }
+
+        for(int i = 0; i < col; i++){
+            if(matrix[0][i] == 0){
+                isfirstrow = true;
+                break;
+            }
+        }
+
+        for(int i = 1; i< row; i++){
+            for(int j = 1; j < col ;j++){
                 if(matrix[i][j] == 0){
-                    q.push({i,j});
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
 
-        while(!q.empty()){
-            int nrow = q.front().first;
-            int ncol = q.front().second;
-            q.pop();
-            for(int i = 0; i < col;i++)
-                matrix[nrow][i] = 0;
+        for(int i = 1; i< row; i++){
+            for(int j = 1; j < col ;j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
 
-            for(int i = 0; i < row;i++)
-                matrix[i][ncol] = 0;
+        if(isfirstrow){
+            for(int i = 0; i < col; i++){
+                matrix[0][i] = 0;
+            }
+        }
+
+        if(isfirstcol){
+            for(int i = 0; i < row; i++){
+                matrix[i][0] = 0;
+            }
         }
     }
 };
