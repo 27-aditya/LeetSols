6c0efd132ac1e80f51037957373b2c8273bd351e
Aditya Kamble <122638989+27-aditya@users.noreply.github.com>
2024-07-18 22:37:30 +0530

    Time: 11 ms (41.35%), Space: 20 MB (8.8%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
+        unordered_map<int,int>map;
         int n = nums.size();
-        int cap = n/3;
-        vector<int>res;
-        unordered_map<int,int>hash;
-
-        for(int num : nums){ 
-            hash[num]++;
-            if(hash[num] > cap && find(res.begin(), res.end(), num) == res.end()) res.push_back(num);
+        for(int i = 0; i < n; i++){
+            map[nums[i]]++;
         }
 
+        int limit = n/3;
+        unordered_set<int>re;
+        for(int num : nums){
+            if(map[num] > limit)
+                re.insert(num);
+        }
+        vector<int>res(re.begin(), re.end());
         return res;
     }
 };
